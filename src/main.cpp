1aa4ed640839f345dcb343cc27bf32fc08c7ac73
Danhlt09 <dn3844146@gmail.com>
2025-08-11 07:54:38 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #define ST7735_DRIVER
-#define TFT_WIDTH  80
-#define TFT_HEIGHT 160
-#define TFT_BL     -1     // Tắt điều khiển Backlight GPIO2
+#define TFT_WIDTH  160
+#define TFT_HEIGHT 80
+#define TFT_BL     -1    // BLK được cấp 3.3V trực tiếp, tắt điều khiển chân BLK
 
 #define TFT_MOSI 18
 #define TFT_SCLK 5
@@ -15,111 +15,26 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
-const int dinoW = 15;
-const int dinoH = 15;
-const int obstacleW = 10;
-const int obstacleH = 15;
-
-int dinoX = 10;
-int dinoY = 80;
-int dinoVY = 0;
-bool isJumping = false;
-
-int obstacleX = 160;
-
-int score = 0;
-
-int lastDinoY = dinoY;
-int lastObstacleX = obstacleX;
-
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
 
   tft.init();
-  tft.setRotation(1);
+  tft.setRotation(1);   // Xoay ngang
   tft.fillScreen(TFT_BLACK);
 
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(0, 0);
-  tft.print("Score: 0");
+  tft.setCursor(10, 10);
+  tft.print("Hello TFT!");
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW && !isJumping) {
-    isJumping = true;
-    dinoVY = -12;
-  }
-
-  if (isJumping) {
-    dinoY += dinoVY;
-    dinoVY += 1;
-
-    if (dinoY >= 80) {
-      dinoY = 80;
-      isJumping = false;
-      dinoVY = 0;
-    }
-  }
-
-  obstacleX -= 6;
-  if (obstacleX < -obstacleW) {
-    obstacleX = 160;
-    score++;
-  }
-
-  bool collided = false;
-  if (obstacleX < dinoX + dinoW && obstacleX + obstacleW > dinoX) {
-    if (dinoY + dinoH > 80) {
-      collided = true;
-    }
-  }
-
-  if (collided) {
+  // Chờ nút bấm để đổi màu màn hình
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    tft.fillScreen(TFT_BLUE);
+    delay(300);
     tft.fillScreen(TFT_BLACK);
-    tft.setCursor(10, 60);
-    tft.setTextSize(3);
-    tft.print("GAME OVER");
-    tft.setCursor(10, 100);
-    tft.setTextSize(2);
-    tft.print("Score: ");
-    tft.print(score);
-    delay(2000);
-
-    obstacleX = 160;
-    score = 0;
-    dinoY = 80;
-    dinoVY = 0;
-    isJumping = false;
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(0, 0);
-    tft.setTextSize(2);
-    tft.print("Score: 0");
-
-    lastDinoY = dinoY;
-    lastObstacleX = obstacleX;
-
-    return;
   }
-
-  // Xóa vùng cũ
-  tft.fillRect(dinoX, lastDinoY, dinoW, dinoH, TFT_BLACK);
-  tft.fillRect(lastObstacleX, 80, obstacleW, obstacleH, TFT_BLACK);
-
-  // Vẽ mới
-  tft.fillRect(dinoX, dinoY, dinoW, dinoH, TFT_WHITE);
-  tft.fillRect(obstacleX, 80, obstacleW, obstacleH, TFT_WHITE);
-
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextSize(2);
-  tft.print("Score: ");
-  tft.print(score);
-
-  lastDinoY = dinoY;
-  lastObstacleX = obstacleX;
-
-  delay(30);
 }
